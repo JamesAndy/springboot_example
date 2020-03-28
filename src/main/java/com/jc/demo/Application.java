3d8c8fe4d1e2d2ed6b1a6c945df944dfe7b99d42
JamesChang <james89926282@gmail.com>
2020-03-28 09:50:15 +0800

    springboot HelloWord

---
+++ src/main/java/com/jc/demo/Application.java
@@ -0,0 +1,13 @@
+package com.jc.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication //->SpringBoot啟動類別
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
